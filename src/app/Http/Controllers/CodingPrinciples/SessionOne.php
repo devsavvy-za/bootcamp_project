3b23dcff72ecb468a973ef0e68cc07831f2d6f3a
Riaan Dreyer <riaan.dreyer@isolve.co.za>
2022-02-01 16:54:08 +0200

    added the coding principles session one file

---
+++ src/app/Http/Controllers/CodingPrinciples/SessionOne.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace App\Http\Controllers\CodingPrinciples;
+
+use App\Http\Controllers\Controller;
+
+class SessionOneController extends Controller
+{
+    /**
+     * Practising naming conventions.
+     */
+    public function letsName()
+    {
+        // reveal
+        $nd =30;
+        $MAX=2;
+
+        // meaningful
+        createDir($p1, $p2);
+        getCustomerIn();
+            getCustomerRs();
+
+        // searchcable
+        MDL = 10;
+        EDATE = now();
+
+        // class nouns
+        $sUSER = new System;
+        $mp = new play;
+
+        // function verbs
+        customer();
+        invs();
+    }
+
+    /**
+     * Practising naming conventions.
+     */
+    public function Naming_is_the_game()
+    {
+        // cute
+        checkThis();
+        bestLog();
+        mailAway();
+
+        // one words
+        fetchResults();
+            getCust_();
+        callAPI();
+
+        // context
+        $name = ' Umbrella Corp ';
+        $secname = 'Jane';
+
+        // naming
+        $larvar = 'A String!';
+        get_new_c_id();
+    }
+
+    /**
+     * Practising naming conventions.
+     */
+    public function FunctionsYo()
+    {
+        // small + one thing + hidden
+        function getInvoice()
+        {
+            // update invoice data
+            [17 lines]
+
+            // get invoice data
+            [8 lines]
+
+            // clean data
+            [12]
+
+            // create pdf
+            [36 lines]
+
+            // send e-mail
+            [23 lines]
+        }
+
+        // dry
+        function get_client() {
+            // get client
+
+            // update invoice data
+            [17 lines]
+
+            // get invoice data
+            [8 lines]
+
+            // clean data
+            [12]
+
+                // create pdf
+            [36 lines]
+
+            // send e-mail
+            [23 lines]
+        }
+    }
+
+    /**
+     * Practising naming conventions.
+     */
+    public function Commentss()
+    {
+        // had to type out the whole alphabet because loops are scary
+        $al = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+        // intent
+        clear_queue_now();
+
+        // loop
+        foreach($arr as $new)
+        {
+            // call
+            clear_queue_now();
+
+            /*
+            send_cool_email();
+             */
+
+            [112 lines]
+        }
+
+
+    }
+
+    /**
+     * Practising naming conventions.
+     */
+    public function forMattinG()
+    {
+        // vertical
+        $num_i = 20;
+        $id = save_new();
+        if($id != 0){
+        foreach($arr as $r)
+        {
+            if($t == $i)
+            {
+
+        }
+        }
+        }
+
+        // class
+        get_invocie();
+        save_Invoice();
+        creat_dir();
+        startSess();
+        getClient();
+        init();
+        getEmails();
+
+        // team rules
+        get_all_clients();
+        fetch_allCustomers();
+    }
+
+
+}
